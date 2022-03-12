55501c4bea3f3e0ca121a920c2eaf5dd00b58b8c
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-12 10:53:49 +0200

    SetUp Color theme.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(BMICalculator());
+void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xFF0A0E21),
+          colorScheme: const ColorScheme.dark(
+            primary: Color(0xFF0A0E21),
+          ),
+          appBarTheme: const AppBarTheme(
+            color: Color(0xFF0A0E21),
+          ),
+          textTheme:
+              const TextTheme(headline1: TextStyle(color: Colors.white))),
+      home: const InputPage(),
     );
   }
 }
 
 class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -21,13 +35,10 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: const Text('BMI CALCULATOR'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
